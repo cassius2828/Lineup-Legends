8e23edda5aaf00baa6a3f7ed76e285cbbd7c0448
Cassius <cassius.reynolds.dev@gmail.com>
2024-06-10 13:22:49 -0700

    organized lineup routes in a router and controller file

---
+++ controllers/lineups.js
@@ -0,0 +1,162 @@
+const PlayerModel = require("../models/player.js");
+const LineupModel = require("../models/lineup.js");
+
+//////////////////////////////
+// Get New Lineup Page
+//////////////////////////////
+const getNewLineup = async (req, res) => {
+  let value5Players;
+  let value4Players;
+  let value3Players;
+  let value2Players;
+  let value1Players;
+
+  try {
+    value5Players = await PlayerModel.aggregate([
+      { $match: { value: 5 } },
+      { $sample: { size: 5 } },
+    ]);
+    value4Players = await PlayerModel.aggregate([
+      { $match: { value: 4 } },
+      { $sample: { size: 5 } },
+    ]);
+    value3Players = await PlayerModel.aggregate([
+      { $match: { value: 3 } },
+      { $sample: { size: 5 } },
+    ]);
+    value2Players = await PlayerModel.aggregate([
+      { $match: { value: 2 } },
+      { $sample: { size: 5 } },
+    ]);
+    value1Players = await PlayerModel.aggregate([
+      { $match: { value: 1 } },
+      { $sample: { size: 5 } },
+    ]);
+  } catch (err) {
+    console.log(`Error randomizing players: ${err}`);
+  }
+
+  res.render("lineups/new.ejs", {
+    value5Players,
+    value4Players,
+    value3Players,
+    value2Players,
+    value1Players,
+  });
+};
+
+//////////////////////////////
+// Post New Lineup
+//////////////////////////////
+const postNewLineup = async (req, res) => {
+  const playerIds = req.body["playerIds[]"] || [];
+  const [pg, sg, sf, pf, c] = playerIds;
+
+  const lineupData = {
+    pg,
+    sg,
+    sf,
+    pf,
+    c,
+    featured: false,
+    owner: req.session.user?._id,
+  };
+
+  try {
+    // Checks to ensure lineup is within the $15 value range
+    const selectedPlayers = await PlayerModel.find({
+      _id: { $in: playerIds },
+    });
+    const totalValue = selectedPlayers.reduce(
+      (acc, player) => acc + player["value"],
+      0
+    );
+    if (totalValue > 15)
+      return res
+        .status(500)
+        .send(
+          "Lineup funds were overdrawn. Lineup could not be created. Please try again"
+        );
+
+    await LineupModel.create(lineupData);
+    console.log("Lineup created successfully");
+    res.redirect(`${req.session.user._id}`);
+  } catch (err) {
+    console.error("Error creating lineup:", err);
+    res.status(500).send("Error occurred in the lineup creation process");
+  }
+};
+
+//////////////////////////////
+// Get Edit Lineup Page
+//////////////////////////////
+const getEditLineup = async (req, res) => {
+  const selectedLineup = await LineupModel.findById(req.params.lineupId)
+    .populate("pg")
+    .populate("sg")
+    .populate("sf")
+    .populate("pf")
+    .populate("c");
+
+  res.render("lineups/edit.ejs", { lineup: selectedLineup });
+};
+
+//////////////////////////////
+// Get Gamble Lineup Page
+//////////////////////////////
+const getGambleLineup = async (req, res) => {
+  const lineup = await LineupModel.findById(req.params.lineupId);
+  const playerToGamble = await PlayerModel.findById(req.params.playerId).populate();
+  console.log(playerToGamble);
+  res.render("lineups/gamble.ejs", {
+    player: playerToGamble,
+    lineup
+  });
+};
+
+//////////////////////////////
+// Delete Lineup
+//////////////////////////////
+const deleteLineup = async (req, res) => {
+  const ownerId = req.session.user._id;
+  const lineupId = req.params.lineupId;
+  try {
+    const selectedLineup = await LineupModel.findById(lineupId);
+
+    if (selectedLineup.owner.equals(ownerId)) {
+      await LineupModel.findByIdAndDelete(lineupId);
+      console.log("Lineup deleted successfully");
+      res.redirect(`/lineups/${ownerId}`);
+    } else {
+      res.status(403).send("You do not have permission to delete this lineup.");
+    }
+  } catch (err) {
+    console.error("Error deleting lineup:", err);
+    res.status(500).send("Error occurred in the lineup deletion process");
+  }
+};
+
+//////////////////////////////
+// Get User Lineups
+//////////////////////////////
+const getUserLineups = async (req, res) => {
+  const userLineups = await LineupModel.find({ owner: req.session.user._id })
+    .populate("pg")
+    .populate("sg")
+    .populate("sf")
+    .populate("pf")
+    .populate("c");
+
+  console.log(userLineups);
+
+  res.render("lineups/index.ejs", { lineups: userLineups });
+};
+
+module.exports = {
+  getNewLineup,
+  postNewLineup,
+  getEditLineup,
+  getGambleLineup,
+  deleteLineup,
+  getUserLineups,
+};
--- routers/lineups-router.js
+++ routers/lineups-router.js
@@ -1,157 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const lineupCtrl = require("../controllers/lineups.js");
-const PlayerModel = require("../models/player.js");
-const LineupModel = require("../models/lineup.js");
 
 //////////////////////////////
 // GET /new - Create New Lineup Page
 //////////////////////////////
-router.get("/new", async (req, res) => {
-  let value5Players;
-  let value4Players;
-  let value3Players;
-  let value2Players;
-  let value1Players;
-
-  try {
-    value5Players = await PlayerModel.aggregate([
-      { $match: { value: 5 } },
-      { $sample: { size: 5 } },
-    ]);
-    value4Players = await PlayerModel.aggregate([
-      { $match: { value: 4 } },
-      { $sample: { size: 5 } },
-    ]);
-    value3Players = await PlayerModel.aggregate([
-      { $match: { value: 3 } },
-      { $sample: { size: 5 } },
-    ]);
-    value2Players = await PlayerModel.aggregate([
-      { $match: { value: 2 } },
-      { $sample: { size: 5 } },
-    ]);
-    value1Players = await PlayerModel.aggregate([
-      { $match: { value: 1 } },
-      { $sample: { size: 5 } },
-    ]);
-  } catch (err) {
-    console.log(`Error randomizing players: ${err}`);
-  }
-
-  res.render("lineups/new.ejs", {
-    value5Players,
-    value4Players,
-    value3Players,
-    value2Players,
-    value1Players,
-  });
-});
+router.get("/new", lineupCtrl.getNewLineup);
 
 //////////////////////////////
 // POST /new - Create New Lineup
 //////////////////////////////
-router.post("/new", async (req, res) => {
-  const playerIds = req.body["playerIds[]"] || [];
-  const [pg, sg, sf, pf, c] = playerIds;
-
-  const lineupData = {
-    pg,
-    sg,
-    sf,
-    pf,
-    c,
-    featured: false,
-    owner: req.session.user?._id,
-  };
-
-  try {
-    // Checks to ensure lineup is within the $15 value range
-    const selectedPlayers = await PlayerModel.find({
-      _id: { $in: playerIds },
-    });
-    const totalValue = selectedPlayers.reduce(
-      (acc, player) => acc + player["value"],
-      0
-    );
-    if (totalValue > 15)
-      return res
-        .status(500)
-        .send(
-          "Lineup funds were overdrawn. Lineup could not be created. Please try again"
-        );
-
-    await LineupModel.create(lineupData);
-    console.log("Lineup created successfully");
-    res.redirect(`${req.session.user._id}`);
-  } catch (err) {
-    console.error("Error creating lineup:", err);
-    res.status(500).send("Error occurred in the lineup creation process");
-  }
-});
+router.post("/new", lineupCtrl.postNewLineup);
 
 //////////////////////////////
-// GET /:lineupId/edit - Edit Lineup
+// GET /:lineupId/edit - Edit Lineup Page
 //////////////////////////////
-router.get("/:lineupId/edit", async (req, res) => {
-  const selectedLineup = await LineupModel.findById(req.params.lineupId)
-    .populate("pg")
-    .populate("sg")
-    .populate("sf")
-    .populate("pf")
-    .populate("c");
-
-  res.render("lineups/edit.ejs", { lineup: selectedLineup });
-});
+router.get("/:lineupId/edit", lineupCtrl.getEditLineup);
 
 //////////////////////////////
-// GET /:lineupId/gamble - gamble Lineup
+// GET /:lineupId/gamble - Gamble Lineup Page
 //////////////////////////////
-router.get("/:lineupId/gamble/:playerId", async (req, res) => {
-    const lineup = await LineupModel.findById(req.params.lineupId)
-  const playerToGamble = await PlayerModel.findById(
-    req.params.playerId
-  ).populate();
-  console.log(playerToGamble);
-  res.render("lineups/gamble.ejs", {
-    player: playerToGamble,
-    lineup
-  });
-});
-//////////////////////////////
-// DELETE /:lineupId/edit - delete Lineup
-//////////////////////////////
-router.delete("/:lineupId/edit", async (req, res) => {
-  const ownerId = req.session.user._id;
-  const lineupId = req.params.lineupId;
-  try {
-    const selectedLineup = await LineupModel.findById(lineupId);
+router.get("/:lineupId/gamble/:playerId", lineupCtrl.getGambleLineup);
 
-    if (selectedLineup.owner.equals(ownerId)) {
-      await LineupModel.findByIdAndDelete(lineupId);
-      console.log("Lineup deleted successfully");
-      res.redirect(`/lineups/${ownerId}`);
-    } else {
-      res.status(403).send("You do not have permission to delete this lineup.");
-    }
-  } catch (err) {
-    console.error("Error deleting lineup:", err);
-    res.status(500).send("Error occurred in the lineup deletion process");
-  }
-});
+//////////////////////////////
+// DELETE /:lineupId/edit - Delete Lineup
+//////////////////////////////
+router.delete("/:lineupId/edit", lineupCtrl.deleteLineup);
+
 //////////////////////////////
 // GET /:ownerId - View User Lineups
 //////////////////////////////
-router.get("/:ownerId", async (req, res) => {
-  const userLineups = await LineupModel.find({ owner: req.session.user._id })
-    .populate("pg")
-    .populate("sg")
-    .populate("sf")
-    .populate("pf")
-    .populate("c");
+router.get("/:ownerId", lineupCtrl.getUserLineups);
 
-  console.log(userLineups);
-
-  res.render("lineups/index.ejs", { lineups: userLineups });
-});
 module.exports = router;
