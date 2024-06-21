2bcdcb77acb89171d564a1e1d6bcb63f1dafa40b
Cassius <cassius.reynolds.dev@gmail.com>
2024-06-20 19:32:56 -0700

    responsive styles and rendering logic done for quick menu icon

--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -1,4 +1,5 @@
 const sideHamburger = document.querySelector("#sidenav");
+const sideHamburgerMobile = document.querySelector("#sidenav.mobile");
 const bar = document.querySelector("#sidenav .bar");
 const sidebar = document.querySelector(".sidebar");
 // change names
@@ -9,14 +10,33 @@ const notiModal = document.getElementById("notifications-modal");
 console.log(sideHamburger);
 
 sideHamburger.addEventListener("click", (e) => {
+  // if(sidebar.classList.contains('move')){
+  //     sideHamburger.style.right = '0'
+  // } else {
+  //   sideHamburger.style.right = '-14rem'
+
+  // }
   bar?.classList.toggle("close");
   sidebar?.classList.toggle("move");
+
   // lineupContainer?.classList.toggle("full");
   // titleInfo?.classList.toggle("full");
-  console.log("closed");
-  console.log(e.target);
-});
 
+});
+sideHamburgerMobile.addEventListener("click", (e) => {
+  // if(sidebar.classList.contains('move')){
+  //     sideHamburger.style.right = '0'
+  // } else {
+  //   sideHamburger.style.right = '-14rem'
+
+  // }
+  bar?.classList.toggle("close");
+  sidebar?.classList.toggle("move");
+
+  // lineupContainer?.classList.toggle("full");
+  // titleInfo?.classList.toggle("full");
+
+});
 notificationEl.addEventListener("click", () => {
   notiModal?.classList.toggle("hide");
 });
